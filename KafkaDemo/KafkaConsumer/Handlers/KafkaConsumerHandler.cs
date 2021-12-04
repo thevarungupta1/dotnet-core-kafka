b71bf0ab8519eca2cac96af994ecd022228000ba
Varun Gupta <thevarungupt1@gmail.com>
2021-12-04 10:34:33 +0530

    Initial Commit

---
+++ KafkaDemo/KafkaConsumer/Handlers/KafkaConsumerHandler.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+using Microsoft.Extensions.Hosting;
+
+namespace KafkaConsumer.Handlers
+{
+    public class KafkaConsumerHandler: IHostedService
+    {
+        private readonly string topic = "simple_topics";
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            var config = new ConsumerConfig
+            {
+                GroupId = "st_consumer_group",
+                BootstrapServers = "localhost:9092",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+            using (var builder = new ConsumerBuilder<Ignore, string>(config).Build())
+            {
+                builder.Subscribe(topic);
+                var cancelToken = new CancellationTokenSource();
+                try
+                {
+                    while(true)
+                    {
+                        var consumer = builder.Consume(cancellationToken);
+                        Console.WriteLine($"message: { consumer.Message.Value} received from {consumer.TopicPartitionOffset}");
+
+                    }
+                }
+                catch(Exception ex)
+                {
+                    builder.Close();
+                }
+            }
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ KafkaDemo/KafkaProducer/Controllers/KafkaProducerController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+
+namespace KafkaProducer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KafkaProducerController : ControllerBase
+    {
+        private readonly ProducerConfig config = new ProducerConfig
+        {
+            BootstrapServers = "localhost:9092"
+        };
+        private readonly string topic = "simple_topics";
+
+        [HttpPost]
+        public IActionResult Post([FromQuery] string message)
+        {
+            return Created(string.Empty, SendToKafka(topic, message));
+        }
+
+        public Object SendToKafka(string topic, string message)
+        {
+           using(var producer = new ProducerBuilder<Null, string>(config).Build())
+            {
+                try
+                {
+                    return producer.ProduceAsync(topic, new Message<Null, string> { Value = message })
+                        .GetAwaiter()
+                        .GetResult();
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine($"Oops, someting went wrong: {ex}");
+                }
+            }
+            return null;
+        }
+    }
+}
